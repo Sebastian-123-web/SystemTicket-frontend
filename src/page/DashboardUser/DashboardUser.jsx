7460df06ada01d9be3d64290e40e491cd36635a0
Sebastian-123-web <sebastian09_26@hotmail.com>
2023-09-22 02:58:47 -0500

    RB | Se completo Login y se esta estructurando el Dashboard

--- src/page/DashboardUser/DashboardUser.jsx
+++ src/page/DashboardUser/DashboardUser.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
 
+import { Card, List, ListItem, ListItemPrefix } from '@material-tailwind/react'
+import { TicketIcon, PlusCircleIcon, PowerIcon } from '@heroicons/react/24/solid'
+
 export const DashboardUser = () => {
-  return (
-    <div>DashboardUser</div>
-  )
+    return (
+        <div className='w-full h-screen bg-[#f3f5f9] flex items-center p-6'>
+            <Card className='h-[calc(100vh-50px)] w-[300px] p-7 flex justify-between items-center flex-col '>
+                <div>Aqui ira un logo :3</div>
+
+                <List>
+                    <ListItem>
+                        <ListItemPrefix>
+                            <TicketIcon className="h-5 w-5" />
+                        </ListItemPrefix>
+                        Mis Ticket
+                    </ListItem>
+                    <ListItem>
+                        <ListItemPrefix>
+                            <PlusCircleIcon className="h-5 w-5" />
+                        </ListItemPrefix>
+                        Crear Ticket
+                    </ListItem>
+                    <ListItem>About dev</ListItem>
+                </List>
+
+                <List>
+                    <ListItem>
+                        <ListItemPrefix>
+                            <PowerIcon className="h-5 w-5" />
+                        </ListItemPrefix>
+                        Log out
+                    </ListItem>
+                </List>
+            </Card>
+        </div>
+    )
 }
--- src/page/Login/Login.jsx
+++ src/page/Login/Login.jsx
@@ -1,14 +1,64 @@
-import { Button, Input, Typography } from "@material-tailwind/react";
+import { Button, Input, Spinner, Typography } from "@material-tailwind/react";
+import { useEffect, useState } from "react";
+
+import { useLoginContext } from '../../context/LoginContext'
+import { Navigate } from "react-router-dom";
 
 export const Login = () => {
+
+    // OBTENER DATOS DE LOS INPUTS
+    const [ dataLogin, setDataLogin ] = useState({
+        email : '',
+        password : ''
+    })
+    const handleCollectLoginUser = (e) => {
+        const { name, value } = e.target
+        setDataLogin((l) => ({
+            ...l,
+            [name] : value
+        }))
+        console.log(dataLogin)
+    }
+
+    // VALIDAR INICIO DE SESION
+    const { login } = useLoginContext()
+    const handleLoginUser = (collectLogin) => {
+        login(collectLogin)
+        Navigate('/dashboard')
+    }
+
+    // SPINNER DE CARGA
+    const [ loading, setLoading ] = useState(true)
+    useEffect(() => {
+        setTimeout(()=>{ setLoading(false) },1000)
+    },[] )
+
     return (
         <div className="w-full h-screen flex justify-center items-center">
-            <div className="w-[400px] bg-white p-5 grid gap-5">
-                <Typography variant="h5" color="blue-gray">Sign Up</Typography>
-                <Input label="Correo" type="email" />
-                <Input label="Contraseña" type="password" />
-                <Button>Iniciar Sesion</Button>
-            </div>
+            {
+                loading ? (
+                    <Spinner className="w-12 h-12" />
+                ) : (
+                    <div className="w-[400px] bg-white p-5 grid gap-5">
+                        <Typography variant="h5" color="blue-gray">Sign Up</Typography>
+                        <Input 
+                            label="Correo"
+                            type="email"
+                            name="email"
+                            onChange={handleCollectLoginUser}
+                        />
+                        <Input 
+                            label="Contraseña" 
+                            type="password"
+                            name="password"
+                            onChange={handleCollectLoginUser}
+                        />
+                        <Button 
+                            onClick={()=>handleLoginUser(dataLogin)}    
+                        >Iniciar Sesion</Button>
+                    </div>
+                )
+            }
         </div>
     )
 }
