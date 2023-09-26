ee4a877a40cc831c1e01f7571bbba5348a5e048e
Rodrigo Sebastian <69646937+Sebastian-123-web@users.noreply.github.com>
2023-09-26 21:52:20 +0000

    Se esta ordenando el dashaboard

--- src/page/DashboardUser/DashboardUser.jsx
+++ src/page/DashboardUser/DashboardUser.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react'
 
 // COMPONENTES DE MATERIAL TAILWIND
-import { Card, Spinner } from '@material-tailwind/react'
+import { Spinner } from '@material-tailwind/react'
 import { Navegation } from '../../components/Navegation/Navegation'
+import { Outlet } from 'react-router-dom'
 
 export const DashboardUser = () => {
 
@@ -21,14 +22,7 @@ export const DashboardUser = () => {
                     <div className='w-full h-full flex'>
                         <Navegation />
                         <div className='w-full py-6 pr-6'>
-                            <Card className='h-[100px]'>
-                                hola
-                            </Card>
-                            <div className='h-[calc(100%-124px)] mt-6 grid grid-cols-12 grid-rows-6 gap-6'>
-                                <Card className='col-span-9 row-span-1 p-5'>card 1</Card>
-                                <Card className='col-span-3 row-span-6 p-5'>Noticias</Card>
-                                <Card className='col-span-9 row-span-5 p-5'>card 3</Card>
-                            </div>
+                            <Outlet />
                         </div>
                     </div>
                 )
--- src/page/DashboardUser/MyTicket/MyTicket.jsx
+++ src/page/DashboardUser/MyTicket/MyTicket.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
 
+import { Card, Input } from '@material-tailwind/react'
+
 export const MyTicket = () => {
   return (
-    <div>MyTicket</div>
+    <>
+      <Card className='h-[100px]'>
+          hola
+      </Card>
+      <div className='h-[calc(100%-124px)] mt-6 grid grid-cols-12 grid-rows-6 gap-6'>
+          <Card className='col-span-9 row-span-1 p-5'>
+            <div className='flex flex-rows' >
+              <h1>Ticket Generados</h1>
+              <div className=' relative '>
+                <Input 
+                  placeholder='Buscar' 
+                  className='w-30'
+                  />
+                <div>
+                  
+                </div>
+              </div>
+            </div>
+          </Card>
+          <Card className='col-span-3 row-span-6 p-5'>Noticias</Card>
+          <Card className='col-span-9 row-span-5 p-5'>card 3</Card>
+      </div>
+    </>
   )
 }
