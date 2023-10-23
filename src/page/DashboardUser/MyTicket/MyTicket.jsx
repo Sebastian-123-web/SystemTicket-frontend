6d28080fab51bb7f7f77e0afd391fd399e2d5b28
Sebastian-123-web <sebastian09_26@hotmail.com>
2023-10-23 17:01:17 -0500

    RB | Feat(Navegation.jsx):Se prepara la navegacion para ser adaptada a otros dispositivos

--- src/page/DashboardUser/MyTicket/MyTicket.jsx
+++ src/page/DashboardUser/MyTicket/MyTicket.jsx
@@ -115,23 +115,21 @@ export const MyTicket = () => {
                         </tbody>
                     </table> */}
 
-                    <div className='flex flex-row items-center h-[50px] text-black bg-[#ecedf1]'>
+                    <div className='flex flex-row items-center h-[50px] rounded-t-lg text-black bg-[#ecedf1]'>
                         <div className='w-[50px] flex justify-center'>#</div>
-                        <div className='w-[50%] text-left pl-5'>Descripcion</div>
-                        <div className='w-[50%] flex flex-row justify-between'>
-                            <div className='w-[120px] hidden md:flex justify-center'>Importancia</div>
-                            <div className='w-[120px] hidden xl:flex justify-center'>Estado</div>
+                        <div className='w-[50%] text-left px-5'>Descripcion</div>
+                        <div className='w-[50%] flex flex-row justify-between items-center'>
+                            <div className='w-[120px] h-[26px] hidden md:flex justify-center'>Importancia</div>
+                            <div className='w-[120px] h-[24px] hidden xl:flex justify-center'>Estado</div>
                             <div className='w-[78px] hidden 2xl:flex justify-center'>Dispositivo</div>
-                            <div className='w-[50px]'></div>
+                            <div className='w-[50px] flex justify-center'>...</div>
                         </div>
                     </div>
                     <div className='flex flex-row items-center border-t py-3'>
-                        <div className='w-[50px] flex justify-center'>
-                            <Typography color="blue-gray" variant="h6" >1</Typography>
-                        </div>
-                        <div className='w-[50%] pl-5'>
+                        <div className='w-[50px] flex justify-center'>1</div>
+                        <div className='w-[50%] text-left px-5'>
                             <Typography color="blue-gray" variant="h6" >Error en SIDIGE</Typography>
-                            <Typography variant="small" color="blue-gray" className="font-normal">
+                            <Typography variant="small" color="blue-gray" className="font-normal whitespace-nowrap overflow-hidden text-ellipsis">
                                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
                             </Typography>
                         </div>
@@ -160,7 +158,7 @@ export const MyTicket = () => {
                             <div className='w-[78px] hidden 2xl:flex justify-center'>
                                 <ComputerDesktopIcon className='w-10 h-10 text-[#263238]' />
                             </div>
-                            <div className='w-[50px]'>
+                            <div className='w-[50px] flex justify-center'>
                                 <Menu>
                                     <MenuHandler>
                                         <IconButton variant="text">
